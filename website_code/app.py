fd4b8884e68ee1bb7479a4979d793922aeaf89e3
Mikey Dodson <mikeyleedodson@gmail.com>
2026-01-26 10:45:53 +0000

    InitialCommit

---
+++ website_code/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/deposit')
+def deposit():
+    return render_template('deposit.html')
+
+@app.route('/widthdraw')
+def widthdraw():
+    return render_template('widthdraw.html')
+
+@app.route('/transfer')
+def transfer():
+    return render_template('transfer.html')
+
+@app.route('/exchange_rates')
+def exchange_rates():
+    return render_template('exchange_rates.html')
+
+
+
+if __name__ == '__main__':
+      app.run(host='127.0.0.1', port=5000, debug=True)
--- website_code/static/js/burgerMenu.js
+++ website_code/static/js/burgerMenu.js
@@ -1,11 +1,13 @@
 
-  const burgerMenu = document.getElementById("burgerMenu");
-  const mainNav = document.getElementById("navBar");
-
-  if (burgerMenu && mainNav) {
+const burgerMenu = document.getElementById("burgerMenu");
+const mainNav = document.getElementById("navBar");
+const ProfileDiv = document.getElementById("ProfileDiv");
+  if (burgerMenu && mainNav&&ProfileDiv) {
     console.info(burgerMenu)
     burgerMenu.addEventListener("click", function () {
       burgerMenu.classList.toggle("active");
       mainNav.classList.toggle("active");
+  
+      ProfileDiv.classList.toggle("active");
     });
   }
